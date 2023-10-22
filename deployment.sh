7406ec36e366aca90392f1f84c7c03aab5c3101f
Arter Tendean <arter.tendean.07@gmail.com>
2023-10-22 14:48:54 +0800

    Fix deployment error

--- deployment.sh
+++ deployment.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 python3 manage.py migrate
 python3 manage.py collectstatic --noinput
-gunicorn config.wsgi:application --bind 0.0.0.0:80
+gunicorn app.wsgi:application --bind 0.0.0.0:80
