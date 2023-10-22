d42befb2c31a889019c9a1169c7d16d3d6f6a1c1
Arter Tendean <arter.tendean.07@gmail.com>
2023-10-22 12:22:32 +0800

    Update secret key settings

--- app/settings.py
+++ app/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY", "s3cr3t-k3y")
+SECRET_KEY = os.environ.get("SECRET_KEY", default="s3cr3t-k3y")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.environ.get("DEBUG", default=0))
