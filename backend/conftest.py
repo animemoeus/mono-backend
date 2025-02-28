d4e3755b3dbf4f52ee140be430ec81a686d770a8
arter.tendean.07 <arter.tendean.07@gmail.com>
2025-02-28 04:13:54 +0000

    Remove TiDB

--- backend/conftest.py
+++ backend/conftest.py
@@ -1,5 +1,4 @@
 import pytest
-from django.test import TestCase
 
 from backend.users.models import User
 from backend.users.tests.factories import UserFactory
@@ -13,7 +12,3 @@ def media_storage(settings, tmpdir):
 @pytest.fixture
 def user(db) -> User:
     return UserFactory()
-
-
-# https://stackoverflow.com/questions/65955251/how-to-use-multiple-databases-for-unit-test-cases-in-django-pytest
-TestCase.databases = {"default", "tidb"}
