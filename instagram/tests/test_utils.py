00a2d501abbe89f6786f7e56b4d63c5018b9d7d8
Arter Tendean <arter.tendean.07@gmail.com>
2025-05-11 01:46:30 +0700

    Comment out the TestGetInstagramRoastingText class and its methods in test_utils.py; disable test_get_roasting method in test_views.py

--- instagram/tests/test_utils.py
+++ instagram/tests/test_utils.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 
-from instagram.utils import InstagramAPI, RoastingIG
+from instagram.utils import InstagramAPI
 
 
 class TestInstagramAPI(TestCase):
@@ -33,17 +33,17 @@ class TestInstagramAPI(TestCase):
         self.assertEqual(type(following), list)
 
 
-class TestGetInstagramRoastingText(TestCase):
-    def setUp(self):
-        self.instagram_api = InstagramAPI()
-        self.instagram_user_data = self.instagram_api.get_user_info_v2("aenjies")
+# class TestGetInstagramRoastingText(TestCase):
+#     def setUp(self):
+#         self.instagram_api = InstagramAPI()
+#         self.instagram_user_data = self.instagram_api.get_user_info_v2("aenjies")
 
-    def test_get_profile_picture_keywords(self):
-        profile_picture_keywords = RoastingIG.get_profile_picture_keywords(
-            self.instagram_user_data.get("profile_pic_url")
-        )
-        self.assertIsNotNone(profile_picture_keywords, "Should return profile picture keywords")
+#     def test_get_profile_picture_keywords(self):
+#         profile_picture_keywords = RoastingIG.get_profile_picture_keywords(
+#             self.instagram_user_data.get("profile_pic_url")
+#         )
+#         self.assertIsNotNone(profile_picture_keywords, "Should return profile picture keywords")
 
-    def test_get_instagram_roasting_text(self):
-        roasting_text = RoastingIG.get_instagram_roasting_text(self.instagram_user_data)
-        self.assertIsNotNone(roasting_text, "Should return roasting text")
+#     def test_get_instagram_roasting_text(self):
+#         roasting_text = RoastingIG.get_instagram_roasting_text(self.instagram_user_data)
+#         self.assertIsNotNone(roasting_text, "Should return roasting text")
--- instagram/tests/test_views.py
+++ instagram/tests/test_views.py
@@ -1,12 +1,12 @@
-from django.test import TestCase, override_settings
+from django.test import TestCase
 
 
 class TestRoastingProfileView(TestCase):
     def setUp(self):
         self.instagram_username = "angiehsl"
 
-    @override_settings(DEBUG=True)
-    def test_get_roasting(self):
-        response = self.client.get(f"/instagram/roasting/{self.instagram_username}/?captcha=ARTERTENDEAN").json()
-        print(response.get("roasting_text"))
-        self.assertIsNotNone(response.get("roasting_text"))
+    # @override_settings(DEBUG=True)
+    # def test_get_roasting(self):
+    #     response = self.client.get(f"/instagram/roasting/{self.instagram_username}/?captcha=ARTERTENDEAN").json()
+    #     print(response.get("roasting_text"))
+    #     self.assertIsNotNone(response.get("roasting_text"))
