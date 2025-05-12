4831b096df2bf66a41f5d7beca5e6dca06d3ecb5
Arter Tendean <arter.tendean.07@gmail.com>
2025-05-12 17:00:35 +0700

    Refactor UserAdmin save_model method to conditionally skip saving during API updates; improve error message formatting

--- instagram/admin.py
+++ instagram/admin.py
@@ -95,6 +95,33 @@ class UserAdmin(SimpleHistoryAdmin):
         update_user_following.delay(obj.instagram_id)
         self.message_user(request, "User following updated")
 
+    def save_model(self, request, obj, form, change):
+        """
+        Override the default save_model method to conditionally skip model saving.
+        This method examines the POST data to determine if the save action is part
+        of an API update operation. If the request contains any of the API update
+        flags (_update-information-from-api, _update-stories-from-api,
+        _update-follower-from-api, _update-following-from-api), the save operation
+        is skipped. Otherwise, it delegates to the parent class's save_model method.
+        Parameters:
+            request (HttpRequest): The request that triggered the save.
+            obj (Model): The model instance to save.
+            form (ModelForm): The form instance used to save the model.
+            change (bool): True if this is a change to an existing object, False if it's a new object.
+        Returns:
+            None
+        """
+
+        if (
+            "_update-information-from-api" in request.POST
+            or "_update-stories-from-api" in request.POST
+            or "_update-follower-from-api" in request.POST
+            or "_update-following-from-api" in request.POST
+        ):
+            return
+
+        super().save_model(request, obj, form, change)
+
 
 @admin.register(Story)
 class StoryAdmin(admin.ModelAdmin):
