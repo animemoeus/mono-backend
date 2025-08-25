649e06aec19051e5f9bec7bb52cf6269e5be7e5b
Arter Tendean <arter.tendean.07@gmail.com>
2025-08-25 13:43:15 +0700

    Revert "Adding authentication app (#700)"
    This reverts commit 54af1521a10ed75a159e0748d51bb173153d59b3.

--- config/settings/base.py
+++ config/settings/base.py
@@ -91,7 +91,6 @@ THIRD_PARTY_APPS = [
     "allauth.socialaccount",
     "django_celery_beat",
     "rest_framework",
-    "rest_framework_simplejwt",
     "django_filters",
     "rest_framework.authtoken",
     "corsheaders",
@@ -109,7 +108,6 @@ THIRD_PARTY_APPS = [
 LOCAL_APPS = [
     "backend.users",
     # Your stuff: custom apps go here
-    "authentication",
     "cinematch",
     "discord",
     "instagram",
@@ -364,7 +362,6 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework.authentication.SessionAuthentication",
         "rest_framework.authentication.TokenAuthentication",
-        "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
     # "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticated",),
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
