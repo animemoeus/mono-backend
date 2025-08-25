63ace88c9700002036175dba93e3c4217bb7ea56
Arter Tendean <arter.tendean.07@gmail.com>
2025-08-25 13:43:29 +0700

    Revert "Adding authentication app (#700)" (#701)
    This reverts commit 54af1521a10ed75a159e0748d51bb173153d59b3.

--- authentication/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from authentication.views import LoginWithGoogleView
-
-app_name = "authentication"
-
-urlpatterns = [
-    path("with-google/", LoginWithGoogleView.as_view(), name="with-google"),
-]
